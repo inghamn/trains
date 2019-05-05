97f6205407ff5e6ae8905f3df0e126f679372706
Cliff Ingham <inghamn@bloomington.in.gov>
2019-05-05 11:43:31 -0400

    Switching to WS websockets library
    This is a standards based library.  If we implement with this, we can
    change server language in the future.

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,13 @@
 import { client } from './client.js';
-const socket = io();
-socket.on('game_state_refresh', function (state) {
-    console.log(state);
-});
+const socket      = new WebSocket('ws://localhost:3000'),
+      heartbeat   = function () {
+                        clearTimeout(this.pingTimeout);
+                        this.pingTimeout = setTimeout(function () { this.terminate; }, 20000);
+                    };
+console.log(socket);
+socket.onopen    = heartbeat;
+socket.onmessage = function (message) { console.log(message); }
+socket.onclose   = function () { clearTimeout(this.pingTimeout); };
 
 document.getElementById('login').addEventListener('submit', function (e) {
     e.preventDefault();
--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 "use strict";
-const express = require('express'),
-      app     = express(),
-      http    = require('http').Server(app),
-      server  = require('socket.io')(http),
-      game    = require('./game');
+const express   = require('express'),
+      app       = express(),
+      http      = require('http').Server(app),
+      WebSocket = require('ws'),
+      wss       = new WebSocket.Server({server: http.listen(3000, function () { console.log('Listening on :3000')})}),
+      game      = require('./game'),
+      heartbeat = setInterval(function () {
+                    wss.clients.forEach(function (socket) {
+                        if (!socket.alive) { return socket.terminate(); }
+                        console.log('sending ping');
+                        socket.alive = false;
+                        socket.ping('ping');
+                    });
+                  }, 5000);
 
 app.use('/', express.static('public'));
-http.listen(3000, function () {
-    console.log('Listening on :3000');
-});
-server.on('connection', function (socket) {
-    console.log('New connection' + socket.handshake.address);
-    server.emit('game_state_refresh', game.state);
-    
-    socket.on('login', function (username) {
-        game.io.login(server, username, socket);
-        console.log('login' + username);
+
+wss.broadcast = function (message) {
+    wss.clients.forEach(function (client) {
+        if (client.readState === WebSocket.OPEN) {
+            client.send(message);
+        }
     });
+}
+
+wss.on('connection', function (socket, request) {
+    console.log('New connection' + request.connection.remoteAddress);
+    socket.alive = true;
+    socket.on('pong', function () {
+        console.log('pong received');
+        this.alive = true;
+    });
+    
+    wss.broadcast(game.state);
+//     server.emit('game_state_refresh', game.state);
+//     
+//     socket.on('login', function (username) {
+//         game.io.login(server, username, socket);
+//         console.log('login' + username);
+//     });
 });
